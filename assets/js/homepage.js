8e297f6275517e8a81e7d5a49a4be3117a5d14d4
Zachary Quintana <zquintana@zingleme.com>
2020-03-02 11:43:29 -0700

    Update home page counter.

---
+++ assets/js/homepage.js
@@ -0,0 +1,37 @@
+(function ($) {
+    function formatNumber(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    $(function() {
+        var $counter = $('#preventable-counter');
+        var limit = 0;
+        var countInterval = setInterval(function() {
+            var currentVal = parseInt($counter.data('count')) || 0;
+            var nextVal = currentVal + 18;
+
+            if (limit > 0 && nextVal >= limit) {
+                clearInterval(countInterval);
+                $counter.text(formatNumber(limit));
+
+                return;
+            }
+
+            $counter
+                .data('count', nextVal)
+                .text(formatNumber(nextVal))
+            ;
+        }, 1);
+
+        $.getJSON('https://us-central1-ess-help.cloudfunctions.net/getPreventableCount').done(function(resp) {
+            limit = resp.count;
+            var currentCount = parseInt($counter.data('count')) || 0;
+
+            if (currentCount >= limit) {
+                clearInterval(countInterval);
+
+                $counter.text(formatNumber(limit));
+            }
+        })
+    });
+})(jQuery);
---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const moment = require('moment');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.getPreventableCount = functions.https.onRequest((request, response) => {
+    const startCount = 11642;
+    const start = moment('2020-03-02 00:00:00', 'YYYY-MM-DD HH:mm:ss');
+    const diff = moment().diff(start, 'hours');
+    const ratePerHour = 4/24; // 4 per 24 hours
+
+    response.set('Access-Control-Allow-Origin', '*');
+    response.json({
+        "count": startCount + (Math.floor(diff * ratePerHour))
+    });
+});
