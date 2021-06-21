18e6eeca1af7d7e7813dadb1655aacfdb2f6c690
atuck22 <60942948+atuck22@users.noreply.github.com>
2021-06-21 09:59:09 -0400

    Create driver-seriously-injured-being-hit-by-tractor-trailer-on-i-75.m

---
+++ content/preventable-tragedies/driver-seriously-injured-being-hit-by-tractor-trailer-on-i-75.m
@@ -0,0 +1,18 @@
+---
+credit:
+  source: Yahoo News
+  url: https://news.yahoo.com/driver-seriously-injured-being-hit-224100770.html
+date: '2021-06-18'
+title: "Driver seriously injured being hit by tractor trailer on I-75"
+---
+Jun. 18—A Douglasville man has been hospitalized with serious injuries after a tractor trailer struck his pickup truck, which was disabled on the shoulder of I-75 following a crash.
+
+Cobb Police say that around 3:30 a.m. Friday, Omar Garcia, 31, failed to maintain his lane while traveling north on I-75 near the Barrett Parkway overpass. Garcia's Ford F-350 collided with a concrete barrier, and the truck barrel-rolled several times before coming to a rest straddling the rightmost lane of the highway and the shoulder.
+
+Investigators believe the Ford's electrical system was disabled by the crash. As Garcia exited the vehicle, a tractor trailer driven by Joseph Caswell, 73, of Thomasville, North Carolina approached in the right lane.
+
+Caswell swerved the tractor trailer onto the highway's shoulder in an unsuccessful attempt to dodge the Ford. He collided with its rear, and the Ford in turn collided with Garcia, who "was accelerated north and entered the travel lanes."
+
+Garcia was transported to Wellstar Kennestone Hospital with serious injuries. Caswell was not injured.
+
+Investigators ask anyone with information about the crash to contact them at 770-499-3987.
